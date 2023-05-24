281b467fb7b4b7e8fd944a0722929395285a70e5
Joel <lavotivinay@gmail.com>
2023-05-24 09:19:01 +0530

    add layout to ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch('http://localhost:8000/records')
+      .then(res => res.json())
+      .then(res => setData(res.data))
+      .catch(error => setError(error.message))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='error'>{error}</p>
+        <div className='card-group'>
+          {data.map(item => (
+            <div className='card'>
+              <p>{item.title}</p>
+              <div className='card-img'></div>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
